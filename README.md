128a85b907e047ea0106832b07d15d9dca415437
khubo <aswinrajv@gmail.com>
2017-08-18 12:35:23 +0530

    added browserify for clientside code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # StoneAge
+
+## How to Run
+
+```
+    npm i
+    npm run start
+```
+
+ P.S: use ` npm run watch` to auto build during development. 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const winston = require('winston')
+const path = require('path')
 
 const PORT = process.env.PORT || 1337
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+// serve the website
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => {
   winston.info(`Server started listening on ${PORT}`)
