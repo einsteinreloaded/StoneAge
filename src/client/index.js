b5a5ea500378181cc038d612cf718f423d6ac00a
Orangespark <changayil1994@gmail.com>
2017-08-20 15:15:54 +0530

    Implemented login Screen and Board Pages

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,28 @@
-console.log('this is working!!')
+
+function requestSession () {
+  let username = document.getElementById('username').value
+  UserAction(username).then(setSession).catch(handleError)
+}
+
+function UserAction (username) {
+  const url = '/user'
+
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ username }),
+    headers: new Headers({ 'Content-Type': 'application/json' })
+  }).then(
+    response => response.text()
+  )
+}
+
+function handleError () {
+  console.log('Game Initialisation Failed!! Please try again')
+}
+
+function setSession (data) {
+  let sessionId = data
+  window.location.href = '/board.html'
+}
+
+Object.assign(window, { requestSession })
