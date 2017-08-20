699615c4c1f555deee2a62d21c0fa1111e108f1e
Orangespark <changayil1994@gmail.com>
2017-08-20 15:15:54 +0530

    login screen and board pages added with server changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const winston = require('winston')
+const bodyParser = require('body-parser')
 const path = require('path')
 
 const PORT = process.env.PORT || 1337
@@ -8,6 +9,13 @@ const app = express()
 // serve the website
 app.use('/', express.static(path.join(__dirname, 'public')))
 
+app.use(bodyParser.json()); // for parsing application/json
+
 app.listen(PORT, () => {
   winston.info(`Server started listening on ${PORT}`)
 })
+
+app.post('/user',function(request,response){
+    var username=request.body.username;
+    response.end(username);
+  });
